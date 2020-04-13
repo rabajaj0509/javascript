e7e9e28a184f87556f582b2484c4e82defba5945
Rahul Bajaj <rahulrb0509@gmail.com>
2020-04-14 02:03:59 +0530

    conditionals

---
+++ conditionals/magicEightBalls.js
@@ -0,0 +1,38 @@
+const userName = "Rahul";
+
+userName === "Rahul" ? console.log(`Hello ${userName}`) : console.log(`Hello!`);
+
+const userQuestion = "Will I become a werewolf tonight?";
+console.log(`${userName} asked: ${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My sources say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+}
+
+console.log(`Prediction: ${eightBall}`);
